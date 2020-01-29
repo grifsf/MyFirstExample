04433c7ffe107a3b479a53ab4c884a8d766496d2
Griff <grifsf@gmail.com>
2020-01-29 11:33:35 -0800

    Takes in two integers now

--- main.cpp
+++ main.cpp
@@ -2,10 +2,13 @@
 
 using std::cin;
 using std::cout;
+using std::cin;
 
 int main()
 {
+    int f_int, s_int;
     cout<<"Hi, please enter two numbers: ";
-
+    cin >> f_int; 
+    cin >> s_int;
     return 0;
 }
